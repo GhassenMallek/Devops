11799aac4c8a3ac6963931a544417d2cece14f60
Ramihb <73407003+Ramihb@users.noreply.github.com>
2022-11-01 22:41:43 +0100

    first jenkinsFile

--- Spring/tpAchatProject/src/main/java/com/esprit/examen/entities/dto/StockDTO.java
+++ Spring/tpAchatProject/src/main/java/com/esprit/examen/entities/dto/StockDTO.java
@@ -4,23 +4,15 @@ import com.esprit.examen.entities.Produit;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.io.Serializable;
 import java.util.Set;
 
 @Getter
 @Setter
 @Builder
-public class StockDTO implements Serializable {
+public class StockDTO {
 	private String libelleStock;
 	private Integer qte;
 	private Integer qteMin;
 	private Set<Produit> produits;
-	public StockDTO(String libelleStock, Integer qte, Integer qteMin) {
-		super();
-		this.libelleStock = libelleStock;
-		this.qte = qte;
-		this.qteMin = qteMin;
-	}
 
 }
